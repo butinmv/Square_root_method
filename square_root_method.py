fa23e177adfea551d6f3891c74a2a19ca7a21318
butinmv <butinmv9722@gmail.com>
2018-03-13 21:35:54 +0700

    Начало создание матриц D и S, без учета перестановки столбцов

--- square_root_method.py
+++ square_root_method.py
@@ -1,17 +1,126 @@
+eps = 1e-10    # погрешность
+
+
 # Проверка матрицы на симметричность
-def checkSymmet(arr):
+def checkSymmet(a):
     n = len(arr)
     flag = 0
     for i in range(n):
         for j in range(n):
-            if arr[i][j] != arr[j][i]:
+            if a[i][j] != a[j][i]:
                 flag = 1
     if flag == 1:
         print("Матрица не симметрична.\nДля метода нужна симметричная матрица.")
+        exit(1)
+
+
+# Функция вывода матрицы на экран
+def displayStartMatrix(a):
+    n = len(a)
+    for i in range(n):
+        line = ""
+        for j in range(n + 1):
+            line += str("%10.5f" % a[i][j]) + "      "
+            if j == n - 1:
+                line += "| "
+        print(line)
+    print("")
+
+
+# Создание матрицы A
+def getMatrixA(arr):
+    a = arr[:, :-1]
+    return a
+
+
+# Функция вывода матрицы A на экран
+def displayMatrix(a):
+    n = len(a)
+    for i in range(n):
+        line = ""
+        for j in range(n):
+            line += str("%10.5f" % a[i][j]) + "      "
+        print(line)
+    print("")
+
+
+# Создание матрицы B
+def getMatrixB(arr):
+    b = arr[:, -1]
+    return b
+
+
+# Функция знака;
+def sign(number):
+    if number > 0:
+        flag = 1
+    else:
+        flag = -1
+    return flag
+
+
+# Создание матрицы D и S
+def getMatrixDS(a):
+    n = len(a)
+
+    d = np.zeros((n, n))
+    s = np.zeros((n, n))
+
+    # стартовые значения
+    d[0][0] = sign(a[0][0])
+    s[0][0] = math.sqrt(abs(a[0][0]))
+    for j in range(1, n):
+        s[0][j] = a[0][j] / s[0][0] / d[0][0]
+
+    for i in range(1, n):
+        sum = 0
+        for l in range(i):
+            sum += s[l][i] * s[l][i] * d[l][l]
+
+        d[i][i] = sign(a[i][i] - sum)
+        s[i][i] = math.sqrt(abs(a[i][i] - sum))
+
+        for j in range(i + 1, n):
+            sum = 0;
+            for l in range(n):
+                sum += s[l][i] * s[l][j] * d[l][l]
+                s[i][j] = (a[i][j] - sum) / s[i][i] / d[i][i]
+    return d, s
+
+
+# Вывод матрицы B на экран
+def displayMatrixB(b):
+    n = len(b)
+    for i in range(n):
+        print("%5.5f" % b[i], end="    ")
+    print("\n")
+
 
 
 if __name__ == "__main__":
     import numpy as np
+    import copy as cp
+    import math
+
     arr = np.loadtxt('input.txt', float) # считываем с файла массив
+    copy_arr = cp.deepcopy(arr)
+
     checkSymmet(arr)
+
+    print("Начальная матрица:")
+    displayStartMatrix(arr)
+
+    print("Матрица A:")
+    a = getMatrixA(copy_arr)
+    displayMatrix(a)
+
+    print("Матрица B:")
+    b = getMatrixB(copy_arr)
+    displayMatrixB(b)
+
+    d, s = getMatrixDS(copy_arr)
+    print("Матрица D:")
+    displayMatrix(d)
+    print("Матрица S:")
+    displayMatrix(s)
 
